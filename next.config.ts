2a656aa623f54f90d336a6f6f5fb06325785240d
ahmadou5 <ahmadlasauwal@gmail.com>
2026-02-16 05:31:29 +0100

    fixes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,31 @@
-import type { NextConfig } from "next";
+// next.config.js
+/** @type {import('next').NextConfig} */
+import type { Configuration } from "webpack";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  webpack: (config: Configuration, { isServer }: { isServer: boolean }) => {
+    if (!isServer) {
+      // Don't resolve 'fs' module on the client to prevent this error on build
+      if (config.resolve) {
+        config.resolve.fallback = {
+          ...config.resolve.fallback,
+          fs: false,
+          net: false,
+          tls: false,
+          crypto: false,
+        };
+      }
+    }
+    return config;
+  },
+  // Add this to help with ESM packages
+  transpilePackages: [
+    "@coinbase/cdp-sdk",
+    "@base-org/account",
+    "@reown/appkit",
+    "@reown/appkit-adapter-wagmi",
+    "@wagmi/connectors",
+  ],
 };
 
-export default nextConfig;
+module.exports = nextConfig;
