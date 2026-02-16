f482f6d4216dd9a480124a7c3fe52e85656c718f
ahmadou5 <ahmadlasauwal@gmail.com>
2026-02-16 05:34:04 +0100

    fixes2

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,6 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
-import type { Configuration } from "webpack";
-
 const nextConfig = {
-  webpack: (config: Configuration, { isServer }: { isServer: boolean }) => {
-    if (!isServer) {
-      // Don't resolve 'fs' module on the client to prevent this error on build
-      if (config.resolve) {
-        config.resolve.fallback = {
-          ...config.resolve.fallback,
-          fs: false,
-          net: false,
-          tls: false,
-          crypto: false,
-        };
-      }
-    }
-    return config;
-  },
-  // Add this to help with ESM packages
+  turbopack: {},
   transpilePackages: [
     "@coinbase/cdp-sdk",
     "@base-org/account",
