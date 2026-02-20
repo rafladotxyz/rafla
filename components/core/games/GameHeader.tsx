26e9e59a7830e71f2979f6b62832be47c883230e
ahmadou5 <ahmadlasauwal@gmail.com>
2026-02-20 04:16:23 +0100

    create room299

--- components/core/games/GameHeader.tsx
+++ components/core/games/GameHeader.tsx
@@ -3,10 +3,24 @@
 import { useAppKitNetwork } from "@reown/appkit/react";
 import { ArrowLeft } from "lucide-react";
 import { useRouter } from "next/navigation";
+import Base from "@/assets/base.png";
+import BaseSepolia from "@/assets/baseSepolia.png";
+import Image from "next/image";
 
-export function GameHeader() {
+export function GameHeader({ gameName }: { gameName?: string }) {
   const router = useRouter();
   const { caipNetwork } = useAppKitNetwork();
+
+  const getNetworkIcon = (name: string) => {
+    switch (name) {
+      case "Base Sepolia":
+        return BaseSepolia;
+      case "Base":
+        return Base;
+      default:
+        return Base; // Fallback icon
+    }
+  };
   return (
     <div className="flex items-center mt-6 justify-between mb-6">
       {/* Go Back Button */}
@@ -19,11 +33,17 @@ export function GameHeader() {
       </button>
 
       {/* Title */}
-      <h1 className="text-xl font-semibold text-[#E8E8E8]">Rafla Draw</h1>
+      <h1 className="text-xl font-semibold text-[#E8E8E8]">{gameName}</h1>
 
       {/* Chain Badge */}
       <div className="flex items-center gap-2 px-4 py-2 rounded-lg border border-[#141414] backdrop-blur-sm">
-        <div className="w-4 h-4 rounded bg-blue-500" />
+        <Image
+          height={20}
+          width={20}
+          src={getNetworkIcon(caipNetwork?.name || "Base")}
+          alt="Base logo"
+          className="rounded-full"
+        />
         <span className="text-sm text-[#E8E8E8]">
           {caipNetwork?.name || "Base"}
         </span>
